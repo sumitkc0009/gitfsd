67a92082c6ac3dbbe2df45cbff4b622beabe930e
sumit <sumit158@amityonline.com>
2024-12-27 21:48:00 +0530

    commiting index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+let movieName = "Deadpool vs wolverine";
+
+var movieDescription = "The new wolverine saga";
+
+const moviePrice = 300;
+
+console.log(400); 
+
+let cast; 
+
+console.log("Sanket Singh", 600);
+
+
+console.log(movieName);
+console.log(movieDescription)
+console.log(moviePrice)
+console.log(cast)
+
+console.log(movieName, movieDescription, moviePrice, cast, 1000);
+
+movieName = "Deadpool vs Wolverine 2";
+console.log(movieName);
+
+let x$y = 10;
+// let x-y = 10;
